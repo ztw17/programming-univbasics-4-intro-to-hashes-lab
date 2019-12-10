d8305f9b5ed09cf223f6efbd7ff75d5b01261554
Zach Weber <weberzt@gmail.com>
2019-12-10 16:35:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  return
 end
 
 def update_counting_hash(hash, key)
